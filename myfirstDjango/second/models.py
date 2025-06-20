809afaa1126877444387e22c1fa99e0ebf4b8ea0
unknown <shubham21491@iiitd.ac.in>
2025-06-20 14:46:28 +0530

    models.py has some more features

--- myfirstDjango/second/models.py
+++ myfirstDjango/second/models.py
@@ -8,6 +8,6 @@ class secondary(models.Model):
     image=models.ImageField(upload_to='second/')
     date_Added=models.DateTimeField(default=timezone.now)
     type=models.CharField(max_length=1,choices=SoftwareType)
-
+    description =models.TextField(max_length=100)
     def __str__(self):
         return self.name
