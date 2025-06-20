59259586ffdd1def2a5f8dbe096cfc787ab0d23c
unknown <shubham21491@iiitd.ac.in>
2025-06-20 16:40:21 +0530

    models.py has been edited

--- myfirstDjango/second/admin.py
+++ myfirstDjango/second/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import secondary
+from .models import secondary, store, softwareReviews, secondCertify
 # Register your models here.
+class secondReviewinline(admin.TabularInline):
+    model=softwareReviews
+    extra=2
 
-admin.site.register(secondary)
+class secondVariety(admin.ModelAdmin):
+    list_display=('name','type')
+    inlines=[secondReviewinline]
+
+class storeadmin(admin.ModelAdmin):
+    list_display=('name','location')
+    # filter_horizontal=('second_variety',)
+
+class secondCertificateAdmin(admin.ModelAdmin):
+    list_display=('second','certificate_number')
+
+admin.site.register(secondary,secondVariety)
+admin.site.register(store,storeadmin)
+admin.site.register(secondCertify,secondCertificateAdmin)
+# admin.site.register(softwareReviews,secondReviewinline)
--- myfirstDjango/second/models.py
+++ myfirstDjango/second/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 class secondary(models.Model):
     SoftwareType=[('M','Mac'),
@@ -11,3 +12,33 @@ class secondary(models.Model):
     description =models.TextField(max_length=100)
     def __str__(self):
         return self.name
+# one to many  
+class softwareReviews(models.Model):
+    second = models.ForeignKey(secondary,on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    rating =models.IntegerField()
+    comment =models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return f"{self.user.username} review for {self.second.name}"
+    
+
+# many to many   
+class store(models.Model):
+    name = models.CharField(max_length=100)
+    location=models.CharField(max_length=100)
+    second_variety = models.ManyToManyField(secondary,related_name='stores')
+    def __str__(self):
+        return self.name
+    
+# one to one
+
+class secondCertify(models.Model):
+    second =models.OneToOneField(secondary,on_delete=models.CASCADE,related_name='Certificate')
+    certificate_number=models.CharField(max_length=100)
+    issued_Date=models.DateTimeField(default=timezone.now)
+    valid_untill=models.DateTimeField()
+    def __str__(self):
+        return f"Certifiacte for {self.name.second}"
+
+
