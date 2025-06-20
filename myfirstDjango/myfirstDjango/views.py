a345432e7ddae59105892d292104f4fe046a6fd7
unknown <shubham21491@iiitd.ac.in>
2025-06-20 12:03:33 +0530

    comments has been deleted in views.py

--- myfirstDjango/myfirstDjango/views.py
+++ myfirstDjango/myfirstDjango/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    # return HttpResponse("home page!")
     return render(request,'websites/index.html')
 def about(request):
     return HttpResponse("about page")
