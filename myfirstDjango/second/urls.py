6db3c37ece095c862e75ac3c9075a5fb7b7f6a85
unknown <shubham21491@iiitd.ac.in>
2025-06-20 15:11:02 +0530

    adding veiws, urls and templates

--- myfirstDjango/second/urls.py
+++ myfirstDjango/second/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.all_soft,name='All_home'),
+    path('<int:second_id>/',views.second_Details,name='second_details'),
     
 ]
--- myfirstDjango/second/views.py
+++ myfirstDjango/second/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import secondary
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def all_soft(request):
     seconds=secondary.objects.all()
     return render(request,'second/all_second.html',{'seconds':seconds})
+
+def second_Details(request,second_id):
+    second=get_object_or_404(secondary,pk=second_id)
+    return render(request,'second/second_details.html',{'second':second})
